786cd21a6b6527fcbbdaf21283d1067a4fe62a50
siwane <camille@taotesting.com>
2018-04-17 16:41:27 +0200

    Add default content type

--- controller/TokenApi.php
+++ controller/TokenApi.php
@@ -46,7 +46,7 @@ class TokenApi extends \tao_actions_CommonModule implements TaoLoggerAwareInterf
             try {
                 $this->responseEncoding = (\tao_helpers_Http::acceptHeader($this->getAcceptableMimeTypes(), $this->getHeader("Accept")));
             } catch (\common_exception_ClientException $e) {
-                $this->returnFailure($e);
+                $this->responseEncoding = "application/json";
             }
         }
 
