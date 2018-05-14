7a4c779f09f37e4beecaf136ce065c3e7d7b6302
siwane <camille@taotesting.com>
2018-05-14 09:58:09 +0200

    Bump to version 2.0.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -94,6 +94,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('0.1.0');
         }
 
-        $this->skip('0.1.0', '1.0.1');
+        $this->skip('0.1.0', '2.0.0');
     }
 }
