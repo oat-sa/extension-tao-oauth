ec28edd020274f42c35b0a06f3f2b22ec8a93577
Aleksej Tikhanovich <aleksej@taotesting.com>
2019-07-16 16:55:47 +0300

    Fixed Unit test

--- test/model/Oauth2TypeTest.php
+++ test/model/Oauth2TypeTest.php
@@ -21,6 +21,9 @@ class Oauth2TypeTest extends TestCase
     /** @var array */
     private $credentials;
 
+
+    private $requestMock;
+
     public function setUp()
     {
         $this->credentials = [
@@ -30,20 +33,28 @@ class Oauth2TypeTest extends TestCase
             'token_type' => 'token_type',
             'grant_type' => 'grant_type'
         ];
+
+        $this->requestMock = $this->createMock(Request::class);
+        $this->requestMock->method('getMethod')->willReturn('GET');
+        $this->requestMock->method('getUri')->willReturn('https://test.uri');
     }
 
     public function testOAuth2Type()
     {
         $Oauth2ServiceMock = $this->createMock(Oauth2Service::class);
         $clientMock = $this->createMock(OAuthClient::class);
-
-        $requestMock = $this->createMock(Request::class);
-        $requestMock->method('getMethod')->willReturn('GET');
-        $requestMock->method('getUri')->willReturn('https://test.uri');
         $clientMock
             ->expects($this->once())
             ->method('request')
-            ->with('GET', 'https://test.uri', array_merge($this->credentials, [ 'body' => null, 'headers' => null]));
+            ->with('GET', 'https://test.uri', [
+                'client_id' => 'client_id',
+                'client_secret' => 'client_secret',
+                'token_url' => 'token_url',
+                'token_type' => 'token_type',
+                'grant_type' => 'grant_type',
+                'body' => null,
+                'headers' => null
+            ]);
 
         $Oauth2ServiceMock->method('getClient')->willReturn($clientMock);
 
@@ -57,7 +68,38 @@ class Oauth2TypeTest extends TestCase
 
         $authType->setCredentials($this->credentials);
 
-        $authType->call($requestMock);
+        $authType->call($this->requestMock);
+    }
+
+    public function testOAuth2TypeEmptySomeCredentials()
+    {
+        $Oauth2ServiceMock = $this->createMock(Oauth2Service::class);
+        $clientMock = $this->createMock(OAuthClient::class);
+
+        unset($this->credentials['token_type'], $this->credentials['grant_type']);
+
+        $clientMock
+            ->expects($this->once())
+            ->method('request')
+            ->with('GET', 'https://test.uri', [
+                'client_id' => 'client_id',
+                'client_secret' => 'client_secret',
+                'token_url' => 'token_url',
+                'body' => null,
+                'headers' => null
+                ]);
+
+        $Oauth2ServiceMock->method('getClient')->willReturn($clientMock);
+
+        $serviceLocatorMock = $this->getServiceLocatorMock([
+            Oauth2Service::SERVICE_ID => $Oauth2ServiceMock
+        ]);
+
+        $authType = new OAuth2AuthType;
+        $authType->setServiceLocator($serviceLocatorMock);
+        $authType->setCredentials($this->credentials);
+
+        $authType->call($this->requestMock);
     }
 
     public function testFaildValidationOAuth2Type()
