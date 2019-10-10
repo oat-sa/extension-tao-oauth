2415cda4e14eb8e6581be8a301ff139d965d41a3
Aleksej Tikhanovich <aleksej@taotesting.com>
2019-10-10 17:16:31 +0300

    Refactored OauthCredentialsFactoryTest

--- test/model/OauthCredentialsFactoryTest.php
+++ test/model/OauthCredentialsFactoryTest.php
@@ -8,6 +8,7 @@ use oat\taoOauth\model\storage\grant\AuthorizationCodeType;
 use oat\taoOauth\model\storage\grant\ClientCredentialsType;
 use oat\taoOauth\model\storage\grant\PasswordType;
 use oat\taoOauth\model\storage\OauthCredentialsFactory;
+use common_exception_ValidationFailed;
 
 /**
  * Class OauthCredentialsFactoryTest
@@ -15,20 +16,47 @@ use oat\taoOauth\model\storage\OauthCredentialsFactory;
 class OauthCredentialsFactoryTest extends TestCase
 {
     /**
-     * @throws \common_exception_ValidationFailed
+     * @throws common_exception_ValidationFailed
      */
-    public function testGettingCredentialTypeByCredentials()
+    public function testGettingClientCredentialTypeByCredentials()
     {
         $OauthCredentialsFactory = new OauthCredentialsFactory();
 
         $clientGrantType = $OauthCredentialsFactory->getCredentialTypeByCredentials([Provider::GRANT_TYPE => ClientCredentialsType::NAME]);
         $this->assertInstanceOf(ClientCredentialsType::class, $clientGrantType);
+    }
+
+    /**
+     * @throws common_exception_ValidationFailed
+     */
+    public function testGettingPasswordTypeByCredentials()
+    {
+        $OauthCredentialsFactory = new OauthCredentialsFactory();
 
         $passwordGrantType = $OauthCredentialsFactory->getCredentialTypeByCredentials([Provider::GRANT_TYPE => PasswordType::NAME]);
         $this->assertInstanceOf(PasswordType::class, $passwordGrantType);
+    }
+
+    /**
+     * @throws common_exception_ValidationFailed
+     */
+    public function testGettingAuthCodeTypeByCredentials()
+    {
+        $OauthCredentialsFactory = new OauthCredentialsFactory();
 
         $codeGrantType = $OauthCredentialsFactory->getCredentialTypeByCredentials([Provider::GRANT_TYPE => AuthorizationCodeType::NAME]);
         $this->assertInstanceOf(AuthorizationCodeType::class, $codeGrantType);
     }
+
+    /**
+     * @throws common_exception_ValidationFailed
+     */
+    public function testGettingAuthCredentialTypeByCredentialsWithException()
+    {
+        $OauthCredentialsFactory = new OauthCredentialsFactory();
+        $this->expectException(common_exception_ValidationFailed::class);
+        $OauthCredentialsFactory->getCredentialTypeByCredentials([Provider::GRANT_TYPE => 'error']);
+    }
+
 
 }
