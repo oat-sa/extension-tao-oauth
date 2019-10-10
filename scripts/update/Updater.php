91aa9579b956b12c1f06c03027fa207d262a41e0
Aleksej Tikhanovich <aleksej@taotesting.com>
2019-10-10 17:54:03 +0300

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -99,7 +99,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('0.1.0');
         }
 
-        $this->skip('0.1.0', '4.2.0');
+        $this->skip('0.1.0', '4.2.1');
 
     }
 }
